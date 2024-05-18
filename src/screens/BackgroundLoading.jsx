e505ab87b9e7673acc87feea4983b6177e0477c9
nindiappc <nindi.nindia44@gmail.com>
2024-05-18 21:10:28 +0700

    Menambahkan Picture and File

---
+++ src/screens/BackgroundLoading.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const BackgroundLoading = () => {
+  return (
+    <View>
+      <Text>BackgroundLoading</Text>
+    </View>
+  );
+};
+
+export default BackgroundLoading;
+
+const styles = StyleSheet.create({});
