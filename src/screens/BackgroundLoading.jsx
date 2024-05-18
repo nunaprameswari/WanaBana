af874465d25dcd2432cf48f922e747a2aee8ef4a
nindiappc <nindi.nindia44@gmail.com>
2024-05-18 21:40:43 +0700

    Editing Color BackgroundLoading

--- src/screens/BackgroundLoading.jsx
+++ src/screens/BackgroundLoading.jsx
@@ -3,12 +3,20 @@ import React from 'react';
 
 const BackgroundLoading = () => {
   return (
-    <View>
-      <Text>BackgroundLoading</Text>
+    <View style={styles.Bgd}>
+      <Text />
     </View>
   );
 };
 
 export default BackgroundLoading;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  Bgd: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f7d200f8',
+  },
+});
