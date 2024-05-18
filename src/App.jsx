8e06a90be6f7690110d6bf39c371e3daf6ea5064
nindiappc <nindi.nindia44@gmail.com>
2024-05-18 22:15:41 +0700

    Menambahkan Stack Screen di App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BackgroundLoading from './screens/BackgroundLoading';
+import Produck from './screens/Produck';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="BacgroundLoading" component={BackgroundLoading} />
+        <Stack.Screen name="Produck" component={Produck} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/BackgroundLoading.jsx
+++ src/screens/BackgroundLoading.jsx
@@ -1,7 +1,10 @@
 import {Image, StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const BackgroundLoading = () => {
+const BackgroundLoading = ({navigation}) => {
+  setTimeout(() => {
+    navigation.replace('Produck');
+  }, 4000);
   return (
     <View style={styles.Bgd}>
       <StatusBar
