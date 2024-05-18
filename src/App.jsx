23a5677acc39e2fc1f1babbcc00d77d912779255
nindiappc <nindi.nindia44@gmail.com>
2024-05-18 21:16:56 +0700

    Memindahkan File App ke Folder src

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import BackgroundLoading from './screens/BackgroundLoading';
 
 const App = () => {
   return (
