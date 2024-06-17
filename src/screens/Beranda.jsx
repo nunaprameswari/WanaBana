d3f7fd5bf0ef36854011a597775b453074460546
nindiappc <nindi.nindia44@gmail.com>
2024-06-17 18:09:30 +0700

    Sedang error

--- src/screens/Beranda.jsx
+++ src/screens/Beranda.jsx
@@ -1,56 +1,14 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  ImageBackground,
-  StatusBar,
-  FlatList,
-  Image,
-  Dimensions,
-} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-
-const screenWidth = Dimensions.get('window').width;
-
-const carouselData = [
-  {
-    id: '1',
-    image: require('../assets/images/Picture_2.jpg'),
-  },
-  {
-    id: '2',
-    image: require('../assets/images/Picture_1.png'),
-  },
-];
-
-const SlideImage = ({item, index}) => {
-  return (
-    <View>
-      <Image source={item.image} style={{height: 200, width: screenWidth}} />
-    </View>
-  );
-};
 
 const Beranda = () => {
   return (
-    <View style={styles.bgd}>
-      <StatusBar
-        barStyle="light-content"
-        hidden={false}
-        backgroundColor="#f7d200f8"
-      />
-      <FlatList data={carouselData} renderItem={SlideImage} horizontal={true} />
+    <View>
+      <Text>Beranda</Text>
     </View>
   );
 };
 
 export default Beranda;
 
-const styles = StyleSheet.create({
-  bgd: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-  },
-});
+const styles = StyleSheet.create({});
