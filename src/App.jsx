b3e2d044a950c573e8e2d69a2a80972e217be41b
nindiappc <nindi.nindia44@gmail.com>
2024-06-17 13:48:08 +0700

    Membuat tab menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,30 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import BackgroundLoading from './screens/BackgroundLoading';
 import Beranda from './screens/Beranda';
 
 const Stack = createNativeStackNavigator();
 const Tabs = createBottomTabNavigator();
 
-const App = () => {
+const MenuTab = () => {
   return (
     <Tabs.Navigator>
+      <Tabs.Screen
+        name="Beranda"
+        component={Beranda}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({size, color}) => {
+            return (
+              <View>
+                <MaterialCommunityIcons name="Grid" size={size} color={color} />
+              </View>
+            );
+          },
+        }}
+      />
     </Tabs.Navigator>
   );
 };
@@ -20,10 +35,14 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Tab" component={MenuTab}  options={{headerShown: false}}/>
+        <Stack.Screen
+          name="Tab"
+          component={MenuTab}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 export default App;
