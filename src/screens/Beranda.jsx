aa36cb6e6d6c2ffd54aa260fdbb2050acc6b88dc
nindiappc <nindi.nindia44@gmail.com>
2024-06-17 17:22:00 +0700

    Menambahkan image

--- src/screens/Beranda.jsx
+++ src/screens/Beranda.jsx
@@ -1,9 +1,25 @@
-import {StyleSheet, Text, View, ImageBackground} from 'react-native';
+import {StyleSheet, Text, View, ImageBackground, StatusBar} from 'react-native';
 import React from 'react';
+
+const Image = [
+  {
+    id: '1',
+    image: require('../assets/images/Picture_2.jpg'),
+  },
+  {
+    id: '2',
+    image: require('../assets/images/Picture_1.png'),
+  },
+];
 
 const Beranda = () => {
   return (
     <View style={styles.bgd}>
+      <StatusBar
+        barStyle="light-content"
+        hidden={false}
+        backgroundColor="#f7d200f8"
+      />
       <ImageBackground
         source={require('../assets/images/Picture_1.png')}
         resizeMode="cover"
