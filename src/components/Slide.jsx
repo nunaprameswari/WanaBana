65baad6233692e758fca8296bbd3d32a494b8362
nindiappc <nindi.nindia44@gmail.com>
2024-06-26 11:21:44 +0700

    Membuat slide foto

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,10 +1,47 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
 
 const Slide = () => {
+  const screenWidth = Dimensions.get('window').width;
+  const [Actived, setActived] = useState(0);
+
+  const DataPicture = [
+    {
+      id: '01',
+      image: require('../assets/images/Picture_2.jpg'),
+    },
+    {
+      id: '02',
+      image: require('../assets/images/Picture_3.jpg'),
+    },
+    {
+      id: '03',
+      image: require('../assets/images/Picture_4.jpg'),
+    },
+  ];
+
+  const pictureItem = ({item, index}) => {
+    return (
+      <View>
+        <Image
+          source={item.image}
+          resizeMode="cover"
+          style={{height: 200, width: screenWidth}}
+        />
+      </View>
+    );
+  };
   return (
     <View>
       <Text>Slide</Text>
+      <FlatList data={DataPicture} />
     </View>
   );
 };
--- src/screens/Beranda.jsx
+++ src/screens/Beranda.jsx
@@ -1,10 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Slide from '../components/Slide';
 
 const Beranda = () => {
   return (
     <View>
-      <Text>Beranda</Text>
+      <Slide />
     </View>
   );
 };
