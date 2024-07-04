d88d8f09d508502764d96584dd0d52edcfe5351c
nindiappc <nindi.nindia44@gmail.com>
2024-07-04 16:19:22 +0700

    Tambah gambar

--- src/screens/ListProduct.jsx
+++ src/screens/ListProduct.jsx
@@ -2,6 +2,20 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 const ListProduct = () => {
+  const DataPicture = [
+    {
+      id: '01',
+      image: require('../assets/images/Picture_2.jpg'),
+    },
+    {
+      id: '02',
+      image: require('../assets/images/Picture_3.jpg'),
+    },
+    {
+      id: '03',
+      image: require('../assets/images/Picture_4.jpg'),
+    },
+  ];
   return (
     <View>
       <Text>ListProduct</Text>
