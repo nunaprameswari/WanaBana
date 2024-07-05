a1c096fea35fab2943353f8a532f199d6ae85839
nindiappc <nindi.nindia44@gmail.com>
2024-07-05 15:01:34 +0700

    Editing Detail Product

--- src/screens/DetailProduct.jsx
+++ src/screens/DetailProduct.jsx
@@ -1,10 +1,17 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import React from 'react';
 
 const DetailProduct = () => {
   return (
     <View style={styles.Bgd}>
-      <View style={styles.BgdDetail}></View>
+      <View style={styles.BgdDetail}>
+        <View style={styles.Screen}>
+          <Image
+            source={require('../assets/images/Banana_Milk(1).png')}
+            style={styles.image}
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -22,5 +29,18 @@ const styles = StyleSheet.create({
     marginTop: 240,
     borderTopLeftRadius: 56,
     borderTopRightRadius: 56,
+    alignItems: 'center',
+  },
+  Screen: {
+    //backgroundColor: 'red',
+    height: 400,
+    width: 400,
+    position: 'absolute',
+    top: -200,
+  },
+  image: {
+    width: 400,
+    height: 400,
+    resizeMode: 'contain',
   },
 });
