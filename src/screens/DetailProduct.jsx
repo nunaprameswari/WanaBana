d1785b55fc28ec8ce9d5d8896de3d2a94fe3eb85
nindiappc <nindi.nindia44@gmail.com>
2024-07-05 14:40:45 +0700

    Editing Detail Product

--- src/screens/DetailProduct.jsx
+++ src/screens/DetailProduct.jsx
@@ -3,12 +3,24 @@ import React from 'react';
 
 const DetailProduct = () => {
   return (
-    <View>
-      <Text>DetailProduct</Text>
+    <View style={styles.Bgd}>
+      <View style={styles.BgdDetail}></View>
     </View>
   );
 };
 
 export default DetailProduct;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  Bgd: {
+    backgroundColor: '#f7d200f8',
+    flex: 1,
+  },
+  BgdDetail: {
+    backgroundColor: '#fff',
+    flex: 1,
+    marginTop: 240,
+    borderTopLeftRadius: 56,
+    borderTopRightRadius: 56,
+  },
+});
