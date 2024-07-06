0d6c0a7609ebb6a4e6bd32602b45e56447caeee3
nindiappc <nindi.nindia44@gmail.com>
2024-07-07 01:07:35 +0700

    Editing berita

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,14 +1,117 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  ImageBackground,
+} from 'react-native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <ScrollView>
+      <StatusBar
+        barStyle="light-content"
+        hidden={false}
+        backgroundColor="rgba(0,0,0,0)"
+      />
+      <Image
+        source={require('../assets/images/Poster.jpg')}
+        style={styles.Img}
+      />
+      <View>
+        <Image
+          source={require('../assets/images/Poster(1).jpg')}
+          style={styles.ImgU}
+        />
+      </View>
+      <View>
+        <Image
+          source={require('../assets/images/Poster(2).jpg')}
+          style={styles.ImgP}
+        />
+        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+          <Text
+            style={{
+              fontSize: 40,
+              borderWidth: 1,
+              backgroundColor: 'green',
+              top: -365,
+              width: 150,
+            }}>
+            WhatsApp
+          </Text>
+        </View>
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <Image
+          source={require('../assets/images/Poster(3).jpg')}
+          style={styles.ImgS}
+        />
+        <Image
+          source={require('../assets/images/Poster(4).jpg')}
+          style={styles.ImgS}
+        />
+        <Image
+          source={require('../assets/images/Poster(5).jpg')}
+          style={styles.ImgS}
+        />
+      </View>
+      <View>
+        <Image
+          source={require('../assets/images/Poster(6).jpg')}
+          style={styles.ImgO}
+        />
+      </View>
+      <View>
+        <Image
+          source={require('../assets/images/Poster(7).jpg')}
+          style={styles.ImgT}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  Img: {
+    width: 360,
+    height: 490,
+    //top: 22,
+  },
+  ImgU: {
+    width: 360,
+    height: 260,
+    //marginLeft: 20,
+    //top: 22,
+  },
+  ImgP: {
+    width: 360,
+    height: 360,
+    //marginLeft: 20,
+    top: 20,
+  },
+  ImgS: {
+    width: 120,
+    height: 120,
+    //marginLeft: 20,
+    top: -16,
+  },
+  ImgO: {
+    width: 360,
+    height: 480,
+    //marginLeft: 20,
+    top: -10,
+  },
+  ImgT: {
+    width: 360,
+    height: 280,
+    //marginLeft: 20,
+    //top: 5,
+  },
+});
